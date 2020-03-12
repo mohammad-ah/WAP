feeef6424801f0638c4740ade5af5b65c8cc80d5
Mohammad <mohamad.ma09@gmail.com>
2020-03-12 14:31:54 -0500

    jquery 1

---
+++ JS/Maze/maze.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $("#start").click(function(){
+
+        function winner() {
+            $("#status").text("You Win :)");
+            $(".boundary").off("mouseenter");
+            $("#maze").off("mouseleave");
+            $(".boundary").addClass("win");
+        }
+
+        function loser() {
+            $(".boundary").addClass("fail");
+            $("#status").text("You Lose :(");
+        }
+
+
+        // reset
+        $(".boundary").removeClass("fail");
+
+        // start game
+        $("#status").text("started.....");
+
+
+        
+
+        $(".boundary").on("mouseenter",function(){
+           loser();
+        });
+        
+
+        $("#maze").on("mouseleave",function(){
+           loser();
+        });
+
+       
+
+        $("#end").on("mouseenter",function(){
+           winner();
+        });
+
+    });
+
+});
